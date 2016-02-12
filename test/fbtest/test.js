c7544e7bcbc0129775c20535d7c492ebdc0879b3
Felipe Buenaño <fesebuv@gmail.com>
2016-02-12 14:42:19 -0500

    Create test.js

---
+++ test/fbtest/test.js
@@ -0,0 +1,25 @@
+function testLogin() {
+      FB.login();
+  // window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id=YOUR_APP_ID&redirect_uri="+uri+"&response_type=token");
+
+  //
+
+  var uri = 'fesebuv.github.io';
+  var appId = '1670352679887073';
+
+  console.log('login atttempt! ');
+
+  try{
+    window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id="+appId+ "&redirect_uri=" + uri + "&response_type=token");
+  } catch(err){
+    FB.login(function(response) {
+      if (response.authResponse) {
+        alert('Success!');
+        window.location.href = 'https://fesebuv.github.io';
+      } else {
+        alert('Login Failed!');
+      }
+    }, {
+      scope: 'email'
+    });
+  }
