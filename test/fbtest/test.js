ed02ab73fd726c5f4c7477432ce07fba57b23134
Felipe Buenaño <fesebuv@gmail.com>
2016-02-12 18:19:49 -0500

    Update test.js

--- test/fbtest/test.js
+++ test/fbtest/test.js
@@ -3,83 +3,14 @@ function testLogin() {
     var uri = 'https://fesebuv.github.io';
     var appId = '1670352679887073';
 
-    console.log('login atttempt! ');
-
-    var fbLogin = false;
-    window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token");
-    window.setTimeout(function() {
-        FB.login(function(response) {
-            fbLogin = true;
-            if (response.authResponse) {
-                alert('Success!');
-                window.location.href = uri;
-            } else {
-                alert('Login Failed!');
-            }
-        });
-    }, 1000);
-
-    window.setTimeout(function() {
-        if (!fbLogin) {
-            window.open("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token");
+    FB.login(function(response) {
+        fbLogin = true;
+        if (response.authResponse) {
+            alert('Success!');
+            window.location.href = uri;
+        } else {
+            alert('Login Failed!');
         }
-    }, 2000);
+    });
 
-
-}
-
-function testLogin1() {
-
-    var uri = 'https://fesebuv.github.io';
-    var appId = '1670352679887073';
-
-    console.log('login atttempt! ');
-
-    window.open(encodeURI("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token"));
-
-
-    try {
-
-        window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token");
-
-
-
-    } catch (err) {
-
-        console.warn(err);
-
-        window.open(encodeURI("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token"));
-
-        try {
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    alert('Success!');
-                    window.location.href = uri;
-                } else {
-                    alert('Login Failed!');
-                }
-            });
-        } catch (r) {
-            console.warn(r);
-        }
-
-    };
-
-
-
-
-    // try {
-
-    // } catch (err) {
-    //     FB.login(function(response) {
-    //         if (response.authResponse) {
-    //             alert('Success!');
-    //             window.location.href = uri;
-    //         } else {
-    //             alert('Login Failed!');
-    //         }
-    //     }, {
-    //         scope: 'email'
-    //     });
-    // }
 }
