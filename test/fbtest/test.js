7d9346b66038f87e7e4b388d5051b782009a102a
Felipe Buenaño <fesebuv@gmail.com>
2016-02-12 14:43:36 -0500

    Update test.js

--- test/fbtest/test.js
+++ test/fbtest/test.js
@@ -1,25 +1,23 @@
 function testLogin() {
-      FB.login();
-  // window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id=YOUR_APP_ID&redirect_uri="+uri+"&response_type=token");
+    FB.login();
 
-  //
+    var uri = 'fesebuv.github.io';
+    var appId = '1670352679887073';
 
-  var uri = 'fesebuv.github.io';
-  var appId = '1670352679887073';
+    console.log('login atttempt! ');
 
-  console.log('login atttempt! ');
-
-  try{
-    window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id="+appId+ "&redirect_uri=" + uri + "&response_type=token");
-  } catch(err){
-    FB.login(function(response) {
-      if (response.authResponse) {
-        alert('Success!');
-        window.location.href = 'https://fesebuv.github.io';
-      } else {
-        alert('Login Failed!');
-      }
-    }, {
-      scope: 'email'
-    });
-  }
+    try {
+        window.location.href = encodeURI("https://www.facebook.com/dialog/oauth?client_id=" + appId + "&redirect_uri=" + uri + "&response_type=token");
+    } catch (err) {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                alert('Success!');
+                window.location.href = 'https://fesebuv.github.io';
+            } else {
+                alert('Login Failed!');
+            }
+        }, {
+            scope: 'email'
+        });
+    }
+}
