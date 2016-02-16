094779afd33e43b70ff9f4642276bbde60cae2a0
Felipe Buenaño <fesebuv@gmail.com>
2016-02-16 03:03:44 -0500

    Update test.js

--- test/fbtest/test.js
+++ test/fbtest/test.js
@@ -1,4 +1,33 @@
-function testLogin() {
+function testLogin(){
+    var uri = 'https://fesebuv.github.io';
+    var appId = '1670352679887073';
+    
+    
+    
+    FB.getLoginStatus(function(response){
+            
+            if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+                // testAPI();
+                window.location.href = uri;
+            } else if (response.status === 'not_authorized') {
+                alert('Please log into this app.');
+                window.location = encodeURI("https://www.facebook.com/dialog/oauth?client_id="+appId+"&redirect_uri="+uri+"&response_type=token");
+                
+            } else {
+                // The person is not logged into Facebook, so we're not sure if
+                // they are logged into this app or not.
+                FB.login();
+                alert('Please log into Facebook.');
+            }
+            
+        });
+    
+}
+
+
+
+function testLogin_() {
 
     var uri = 'https://fesebuv.github.io';
     var appId = '1670352679887073';
