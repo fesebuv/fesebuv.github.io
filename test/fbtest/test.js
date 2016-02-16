c6772fd2c7a500a93a668a75883f22ed685a8e4d
Felipe Buenaño <fesebuv@gmail.com>
2016-02-16 02:47:32 -0500

    Update test.js

--- test/fbtest/test.js
+++ test/fbtest/test.js
@@ -11,6 +11,22 @@ function testLogin() {
         } else {
             alert('Login Failed!');
         }
+        
+        FB.getLoginStatus(function(response){
+            
+            if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+                testAPI();
+            } else if (response.status === 'not_authorized') {
+                console.info('Please log into this app.');
+            } else {
+                // The person is not logged into Facebook, so we're not sure if
+                // they are logged into this app or not.
+                console.info('Please log into Facebook.');
+            }
+            
+        });
+        
     });
 
 }
